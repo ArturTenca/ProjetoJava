64b72eac10bd210da7e37ecfd27d8487456ae15f
artur Tenca <arturtenca1@gmail.com>
2026-05-21 16:52:35 -0300

    add classe project

---
+++ src/main/java/com/projeto/curso/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.projeto.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.projeto.curso.entities.Product;
+import com.projeto.curso.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository ProductRepository;
+	
+	public List<Product> findAll (){
+		return ProductRepository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = ProductRepository.findById(id);
+		return obj.get(); 
+	}
+}
